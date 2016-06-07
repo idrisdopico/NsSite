2d3d7a6e717766aa203c9cbf13b49dda52ce6bb9
Idris Dopico Peña <dopico1996@hotmail.com>
2016-06-07 12:21:19 +0200

    first commit

---
+++ studentsApp/app/app.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    /* App Module */
+    angular.module('md2App', [
+        'controllers',
+        'ngAnimate'
+    ]);
+
+    angular.module('controllers', []);
+
+
+
+})();
