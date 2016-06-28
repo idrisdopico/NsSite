388bd7b654a746e2e3001968e710c67745a059a7
19392 <19392@ma-web.nl>
2016-06-28 13:46:11 +0200

    search done, started styling

--- app/controllers/stations.controller.js
+++ app/controllers/stations.controller.js
@@ -5,12 +5,11 @@
         .module('controllers')
         .controller('StationsController', StationsController);
 
-    function StationsController($http) {
+    function StationsController($http,filterFilter) {
 
         var vm = this;
 
         vm.newStation = {};
-        vm.filtered = [];
 
         vm.setStation = function(currentStation){
             if(vm.currentStation != currentStation)
@@ -33,24 +32,28 @@
         }
 
         vm.allStations = [
-            // {name:"Anton", age:18},
-            // {name:"Joris", age:29},
-            // {name:"Cees", age:19},
-            // {name:"Ryan", age:18},
-            // {name:"Jesse", age:18},
-            // {name:"Idris", age:20},
-            // {name:"Donovan", age:18}
+        ];
+        vm.savedStations = [
         ];
 
         $http.get('data/trainstations.json').then(function(stations){
             vm.allStations = stations.data;
+            vm.savedStations = stations.data;
+
         });
 
         vm.map = {
           center: { latitude: 52, longitude: 5 }, zoom: 7
         };
 
-        //markers hier
+        vm.filtersorting = function(query){
+          vm.allStations = angular.copy(vm.savedStations);
+          this.query = query;
+          vm.allStations = filterFilter(vm.allStations,this.query);
+        }
+
+
+
 
 
     }
